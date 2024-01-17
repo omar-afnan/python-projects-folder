951425f39881b5b5817a93fac83b459e6fd16419
Syed Muhammad Omar Afnan <138956919+omar-afnan@users.noreply.github.com>
2024-01-17 09:56:51 +0400

    Create replacing.py

---
+++ replacing.py
@@ -0,0 +1,12 @@
+#Initialize a string variable word and assign the value Ocygen to it.
+#You now want to fix the typo in the given string.
+#Use the syntax explained above to replace 'c' with 'x' in a new variable word_new
+#Output the updated word_new to console
+
+
+#code
+
+
+word = "Ocygen"
+new_word= word.replace('c','x')
+print(new_word)
